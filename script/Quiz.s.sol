abb6967aeb7c070105830a8e5182d2cb55d39f9a
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-08-07 20:16:57 +0900

    🛠️ Chore : script

--- script/Quiz.s.sol
+++ script/Quiz.s.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "src/Quiz.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        Quiz quiz = new Quiz();
     }
 }
